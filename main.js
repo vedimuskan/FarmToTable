d39022a094dc02ffa08a229fc260574402ae3657
vedimuskan <113710089+vedimuskan@users.noreply.github.com>
2023-01-18 20:50:57 +0530

    Final copy of project.

---
+++ main.js
@@ -0,0 +1,100 @@
+
+
+const nav = document.getElementById('list');
+
+  
+    /**
+     * Easy selector helper function
+     */
+    const select = (el, all = false) => {
+      el = el.trim()
+      if (all) {
+        return [...document.querySelectorAll(el)]
+      } else {
+        return document.querySelector(el)
+      }
+    }
+  
+    /**
+     * Easy event listener function
+     */
+    const on = (type, el, listener, all = false) => {
+      let selectEl = select(el, all)
+      if (selectEl) {
+        if (all) {
+          selectEl.forEach(e => e.addEventListener(type, listener))
+        } else {
+          selectEl.addEventListener(type, listener)
+        }
+      }
+    }
+  
+    /**
+     * Easy on scroll event listener 
+     */
+    const onscroll = (el, listener) => {
+      el.addEventListener('scroll', listener)
+    };
+window.onresize = function(){
+    const list = document.getElementById('navLinks');
+    if(screen.width>700){
+        list.style = "display:block; transition:0.5s"
+        nav.innerHTML = `X`;
+    }
+}
+
+window.onload = function(){
+    const list = document.getElementById('navLinks');
+    if(screen.width<700){
+        list.style = "display:block; transition:0.5s"
+        nav.innerHTML = `X`;
+    }
+    let menuContainer = select('.menu-container');
+
+    if (menuContainer) {
+
+      let menuIsotope = new Isotope(menuContainer, {
+        itemSelector: '.menu-item',
+        layoutMode: 'fitRows'
+      });
+
+      let menuFilters = select('#menu-flters li', true);
+
+      on('click', '#menu-flters li', function(e) {
+        e.preventDefault();
+        menuFilters.forEach(function(el) {
+          el.classList.remove('filter-active');
+        });
+        this.classList.add('filter-active');
+
+        menuIsotope.arrange({
+          filter: this.getAttribute('data-filter')
+        });
+
+      }, true);
+    }
+}
+
+const displayNav = ()=>{
+    const list = document.getElementById('navLinks');
+    if(nav.innerHTML == "X" && screen.width<=700){
+        list.style = "display:none; transition:0.5s ease";
+        nav.innerHTML = `<i class="fa fa-bars" aria-hidden="true"></i>`;
+    }
+    else{
+        list.style = "display:block; font-family: sans-serif; transition:0.5s ease"
+        nav.innerHTML = `X`;
+    }
+}
+
+nav.addEventListener('click',displayNav);
+
+const bg = document.getElementById("body");
+const paybtn = document.getElementById("btnpay");
+
+const paynow = ()=>{
+  img = document.getElementById('upi');
+  img.style = "display: block;"
+}
+
+paybtn.addEventListener('click',paynow);
